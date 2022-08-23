e5214ab74afd2a68952e6727b434f5e8acf9b6ba
manish1485 <mailmanish02@gmail.com>
2022-08-23 15:00:20 -0400

    Manish commit

---
+++ src/test/java/com/Tests/TC01_LoginPageTest.java
@@ -0,0 +1,34 @@
+package com.Tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC01_LoginPageTest {
+
+	@Test
+
+	public void test1() {
+
+		System.setProperty("webdriver.chrome.driver",
+				System.getProperty("user.dir") + "/src/test/resources/Executables/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.findElement(By.name("q")).sendKeys("Automation", Keys.ENTER);
+		driver.quit();
+
+	}
+
+	public void test2() {
+
+		System.setProperty("webdriver.chrome.driver",
+				System.getProperty("user.dir") + "/src/test/resources/Executables/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.findElement(By.name("q")).sendKeys("Selenium", Keys.ENTER);
+		driver.quit();
+
+
+}
